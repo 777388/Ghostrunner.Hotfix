6205f7f041d52938c644c5bbeae21baea0f81ace
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-04-11 22:22:52 -0700

    Update hotfix(ghostrunner).py

--- hotfix(ghostrunner).py
+++ hotfix(ghostrunner).py
@@ -2,5 +2,5 @@ import deprecated
 
 for i in str(deprecated.__path__.extend.__setattr__.__init__.__call__):
     print(chr(ord(i)+42), end="")
-deprecated.__path__.extend.__set__.__init__.__call__ == "D3D device"
+deprecated.__path__.extend.__setattr__.__init__.__call__ == "D3D device"
 d3d_device = (deprecated.__path__.extend.__setattr__.__init__.__call__ == deprecated.__path__.extend.__setattr__.__init__.__call__)
