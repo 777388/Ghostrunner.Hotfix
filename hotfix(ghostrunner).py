56e40ec0a711fbdd38b868d4746c89852a5ca94c
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-04-11 21:55:23 -0700

    Add files via upload

---
+++ hotfix(ghostrunner).py
@@ -0,0 +1,6 @@
+import deprecated
+
+for i in str(deprecated.__path__.extend.__setattr__.__init__.__call__):
+    print(chr(ord(i)+42), end="")
+deprecated.__path__.extend.__set__.__init__.__call__ == "D3D device"
+d3d_device = (deprecated.__path__.extend__setattr__.__init__.__call__ == deprecated.__path__extend.__setattr__.__init__.__call__)
